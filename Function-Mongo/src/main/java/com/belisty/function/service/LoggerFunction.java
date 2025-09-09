c4943a7200c0d4ae4ad74beea03ba44c7a50726e
pcyuen98 <pcyuen98@gmail.com>
2025-09-09 14:00:21 +0800

    updates

---
+++ Function-Mongo/src/main/java/com/belisty/function/service/LoggerFunction.java
@@ -0,0 +1,37 @@
+package com.belisty.function.service;
+
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import com.util.function.service.AzureTableLoggerService;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * Azure Function using Spring Cloud Function style. This will be triggered via
+ * FunctionInvoker.
+ */
+@Component
+@AllArgsConstructor
+public class LoggerFunction {
+
+	private final AzureTableLoggerService loggerService;
+
+	@Bean(name = "loggingtest")
+	public Function<Void, String> execute() {
+		return input -> {
+			
+			String lastLogger = null;
+			try {
+				loggerService.info("loggerTest here");
+				lastLogger =  loggerService.getHistory(1);
+			} catch (Exception e) {
+				loggerService.info(e.getMessage());
+			}
+			return lastLogger;
+		};
+	}
+
+}
