b102e54c64a06a05b6b7d10552c8c96be084abc1
pcyuen98 <pcyuen98@gmail.com>
2025-09-09 13:39:17 +0800

    updates

--- Function-Resource-Logging/src/test/java/com/util/function/service/AzureResourceManagerServicesTest.java
+++ Function-Resource-Logging/src/test/java/com/util/function/service/AzureResourceManagerServicesTest.java
@@ -3,25 +3,28 @@ package com.util.function.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootTest
-class AzureResourceManagerServiceTest {
+class AzureResourceManagerServicesTest {
 
 	@Autowired
 	private AzureResourceManagerService azureResourceManagerService;
 
 	@Test
 	void azureResourceTest() {
-		String json = azureResourceManagerService.getAzureResourceSummary("unknown IP");
-
+		log.info("azureResourceTest test starts");
+		
+		String json = azureResourceManagerService.getAzureResourceSummary("unknown IP");		
 		assertNotNull(json, "Azure resource summary JSON should not be null");
 	}
 
-	@Test
+	//@Test
 	void stopAzureService() {
 		// Note: Only to shutdown service test
 		// String result = azureResourceManagerService.stopAzureService(null);
--- Function-Resource-Logging/src/test/java/com/util/function/service/AzureTableLoggerServiceTest.java
+++ Function-Resource-Logging/src/test/java/com/util/function/service/AzureTableLoggerServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
