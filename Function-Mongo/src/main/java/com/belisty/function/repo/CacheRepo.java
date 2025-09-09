1648d4412e6da4d7252d02548bd96994a87182e9
pcyuen98 <pcyuen98@gmail.com>
2025-09-09 13:59:34 +0800

    Updates

--- Function-Mongo/src/main/java/com/belisty/function/repo/CacheRepo.java
+++
@@ -1,9 +0,0 @@
-package com.belisty.function.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.belisty.function.model.GetCache;
-
-public interface CacheRepo extends MongoRepository<GetCache, String> {
-
-}
